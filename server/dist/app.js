88939d4c2e3df6176c2f35be9d1f55421de1bbff
Eric Wan <ericwan957@gmail.com>
2024-11-14 04:54:26 -0500

    Create different configurations for development and production

--- server/dist/app.js
+++ server/dist/app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import path from "path";
 const PORT = 3000;
 const app = express();
-app.get("/", (req, res) => {
-    res.send("Hello world!");
+app.use(express.static(path.join(import.meta.dirname, "../../client/dist")));
+app.use((req, res, next) => {
+    console.log("HTTP request", req.method, req.url, req.body);
+    next();
+});
+app.get("*", (req, res) => {
+    res.sendFile(path.join(import.meta.dirname, "../../client/dist/index.html"));
 });
 app.listen(PORT, () => {
     console.log(`SimpleILS backend listening on port ${PORT}`);
+    console.log(process.env.NODE_ENV);
 });
 export default app;
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,28 @@
+import cors from "cors";
 import express from "express";
+import path from "path";
 
 const PORT = 3000;
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.use((req, res, next) => {
+  console.log("HTTP request", req.method, req.url, req.body);
+  next();
 });
 
+if (process.env.NODE_ENV === "dev") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+} else if (process.env.NODE_ENV === "prod") {
+  app.use(express.static(path.join(import.meta.dirname, "../../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(import.meta.dirname, "../../client/dist/index.html"));
+  });
+}
+
 app.listen(PORT, () => {
-  console.log(`SimpleILS backend listening on port ${PORT}`);
+  console.log("SimpleILS backend listening on port", PORT);
+  console.log("Running server in mode:", process.env.NODE_ENV);
 });
 
 export default app;
